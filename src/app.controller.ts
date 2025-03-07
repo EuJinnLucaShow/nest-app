5168360fa7085a2c8675e739b3b7543e9ab5f874
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2025-03-07 13:03:35 +0200

    Summary
    src\app.service.ts
    src\app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('cats')
+  findAll(): string {
+    return this.appService.getCats();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getCats(): string {
+    return 'This action returns all cats';
+  }
 }
